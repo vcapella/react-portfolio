c8075554775e36309853594f73755e24eb5da6dc
Victor Capella <90168071+vcapella@users.noreply.github.com>
2021-11-22 15:44:49 -0500

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,20 +2,11 @@ import React from "react";
 import "../styles/Intro.css";
 import myself from "../assets/me_wide.jpeg";
 
-// By importing the Section.css file, it is added to the DOM whenever this component loads
-
-// We can also style a component inside of its JavaScript file by adding style properties to its rendered elements
-// Unlike regular HTML, a JSX style property must be an object instead of a string
-// On a style object, we camelCase all property names, and put all of the values in quotes
-// Non quoted values default to "pixels", e.g. height, margin, padding
-
 const styles = {
   introStyles: {
     background: "white",
   },
 };
-
-// We use JSX curly braces to evaluate the style object on the JSX tag
 
 function Intro() {
   return (
