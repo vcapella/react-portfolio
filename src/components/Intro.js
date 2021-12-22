c9893edb275c48556c8da64e0e6d4d82e9d50d6b
Victor Capella <capella89@outlook.com>
2021-12-21 22:47:37 -0500

    update intro and creation of _redirects file

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -17,21 +17,21 @@ function Intro() {
 
       <h2 className="intro-h2">Hello!</h2>
       <p className="p-intro">
-        Curious by nature, fast learner and gamer on my spare time. I'm Victor a
-        Brazilian web developer currently based in Toronto, Canada.
+        Curious by nature, fast learner and gamer in my spare time. I'm Victor,
+        a Brazilian web developer currently based in Toronto, Canada.
       </p>
       <p className="p-intro">
         I’ve been intrigued by coding for a while and early 2021 I decided to
-        give it a try. From small online courses to articles I like how this
-        universe challenged me and I kept getting more and more interested in
-        all its possibilities. Fast forward to November 2021 and I just got my
+        give it a try. From small online courses to articles, I liked how this
+        universe challenged me and I kept getting more interested in all its
+        possibilities. Fast forward to November 2021 and I just got my
         full-stack certificate from the University of Toronto Coding Bootcamp.
       </p>
       <p className="p-intro">
-        My skillset includes Javascript, HTML, Node.js, React and CSS, including
-        experience with modern frameworks like bootstrap and bulma. I like
-        building things from scratch - like this portfolio - and I’m always
-        eager to learn new things.
+        MMy skill set includes Javascript, HTML, Node.js, React and CSS,
+        including experience with modern frameworks like bootstrap, tailwind and
+        bulma as seen in my projects. I like building things from scratch - like
+        this portfolio - and I’m always eager to learn new things.
       </p>
     </section>
   );
